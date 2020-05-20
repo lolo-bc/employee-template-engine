74d70c1d0cb0c030bc1cb30bdf00e185942f1e81
lolobc <lauren.n.cameron@gmail.com>
2020-05-20 00:06:47 -0400

    push

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -7,8 +7,8 @@ class Engineer extends Employee {
     constructor(github, name, id, email) {
         super(name, id, email);
         this.github = github
-        const enRole = "Engineer"
-        this.role = enRole
+        const role = "Engineer"
+        this.role = role
 
     };
 
